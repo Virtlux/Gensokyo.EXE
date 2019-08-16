3813b29c1b42e3074ba9ccc5df1d9390ee185cca
Virtlux <54161848+Virtlux@users.noreply.github.com>
2019-08-16 15:58:20 +0200

    Update patch.js

--- THBL_01/patch.js
+++ THBL_01/patch.js
@@ -1,13 +1,13 @@
 {
-	"dependencies": [
-		"nmlgc/base_tsa",
-		"nmlgc/script_latin",
-		"nmlgc/western_name_order"
-	],
-	"id": "THBL_01",
-	"servers": [
-		"https://raw.githubusercontent.com/Virtlux/Gensokyo.EXE/master/THBL_01/"
-	],
-	"title": "Touhou Black Label 01 - Desire.EXE",
-	"update": true
+    "id": "THBL_01",
+    "title": "Touhou Black Label 01 - Desire.EXE",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/script_latin",
+        "nmlgc/western_name_order"
+    ],
+    "update": true,
+    "servers": [
+        "https://raw.githubusercontent.com/Virtlux/Gensokyo.EXE/master/THBL_01/"
+    ]
 }
