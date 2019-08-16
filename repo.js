83ec7804ba5ea9b8405f4661c21d0713509ef994
Virtlux <54161848+Virtlux@users.noreply.github.com>
2019-08-16 15:42:37 +0200

    e

--- repo.js
+++ repo.js
@@ -1,11 +1,11 @@
 {
-    "contact": "Virtlux#7039",
-    "id": "Gensokyo.EXE",
-    "patches": {
-        "THBL_01": "Touhou Black Label Modification for Ten Desires"
-    },
-    "servers": [
-        "https://raw.githubusercontent.com/Virtlux/Gensokyo.EXE/master/"
-    ],
-    "title": "Gensokyo Executors"
+	"contact": "Virtlux#7039",
+	"id": "Gensokyo.EXE",
+	"patches": {
+		"THBL_01": "Touhou Black Label 01 - Desire.EXE"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/Virtlux/Gensokyo.EXE/master/"
+	],
+	"title": "Gensokyo Executors"
 }
