f5c6d97a961884aa43e5f1b9cc4025ee7dfaeaaf
Virtlux <54161848+Virtlux@users.noreply.github.com>
2019-08-16 05:07:16 +0200

    Update patch.js

--- THBL_01/patch.js
+++ THBL_01/patch.js
@@ -1,7 +1,13 @@
 {
     "id": "THBL_01",
     "title": "Touhou Black Label 01 - Desire.EXE",
-
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/base_tasofro",
+        "nmlgc/script_latin",
+        "nmlgc/western_name_order"
+    ],
+    "update": true,
     "servers": [
         "https://mirrors.thpatch.net/Gensokyo.EXE/THBL_01/"
     ]
